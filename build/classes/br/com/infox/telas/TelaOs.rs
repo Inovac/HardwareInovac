3372fa2e798331bd93e67aa6b9cd4aad34ce0bef
Inovac Technology <92610555+Inovac@users.noreply.github.com>
2021-10-27 16:56:59 -0300

    Add files via upload

---
+++ build/classes/br/com/infox/telas/TelaOs.rs
@@ -0,0 +1,12 @@
+br.com.infox.telas.TelaOS$1
+br.com.infox.telas.TelaOS$11
+br.com.infox.telas.TelaOS$10
+br.com.infox.telas.TelaOS$6
+br.com.infox.telas.TelaOS$7
+br.com.infox.telas.TelaOS$8
+br.com.infox.telas.TelaOS
+br.com.infox.telas.TelaOS$9
+br.com.infox.telas.TelaOS$2
+br.com.infox.telas.TelaOS$3
+br.com.infox.telas.TelaOS$4
+br.com.infox.telas.TelaOS$5
